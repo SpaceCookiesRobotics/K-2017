ac379e0e0f631b51b38cad5500e9ee496572fd74
VEX1868 Team K <30915286+VEX1868TeamK@users.noreply.github.com>
2017-10-19 17:47:45 -0700

    include competition code

--- Competition.c
+++ Competition.c
@@ -9,6 +9,8 @@
 #pragma config(Motor,  port9,           dr4b4,         tmotorVex393_MC29, openLoop, reversed)
 #pragma config(Motor,  port10,          middleright,   tmotorVex393_HBridge, openLoop, reversed)
 
+#include "../Shared/Competition.h"
+
 int turnvar = 1;
 void setleft (int speed) {
 	motor[middleleft]=speed;
@@ -30,7 +32,7 @@ void turn (int speed) {
 	setright (-1*turnvar*speed);
 }
 
-void auton () {
+void autonomous () {
 	turn (1);
 	wait1Msec (2000);
 	vertical (100);
@@ -45,42 +47,42 @@ void auton () {
 	wait1Msec (500);
 	motor [lift] = 100;
 	wait1Msec (1000);
-  vertical (50);
-  wait1Msec (500);
-  vertical (-100);
-  wait1Msec (1100);
-  turn (-1);
-  wait1Msec (1300);
-  vertical (127);
-  wait1Msec (3200);
+	vertical (50);
+	wait1Msec (500);
+	vertical (-100);
+	wait1Msec (1100);
+	turn (-1);
+	wait1Msec (1300);
+	vertical (127);
+	wait1Msec (3200);
 }
 
-	task main () {
-		int joy_x;            // will hold the X value of the analog stick (choices below)
-		int joy_y;            // will hold the Y value of the analog stick (choices below)
-		int slowbutton = vexRT[Btn5U];       // either 0 or 1 (pressed or not pressed)
-		int slowdown;
+void joystick () {
+	int joy_x;            // will hold the X value of the analog stick (choices below)
+	int joy_y;            // will hold the Y value of the analog stick (choices below)
+	int slowbutton = vexRT[Btn5U];       // either 0 or 1 (pressed or not pressed)
+	int slowdown;
 
-		while(1 == 1) {
-			joy_x = vexRT[Ch4];   // This is the left analog stick.
-			joy_y = vexRT[Ch3];
-			if (slowbutton == 0) {  // If slowbutton is pressed, speed decreases by 5. If not, it stays the same.
-				slowdown = 1;
-			}
-			if (slowbutton == 1) {
-				slowdown = 5;
-			}
-			setleft ((joy_y + joy_x)/slowdown);
-			setright ((joy_y - joy_x)/slowdown);
-
-			// Mobile Goal Lift motor
-			motor[lift] = vexRT[Ch2]/slowdown;
-			// DR4B Claw
-			motor[claw] = vexRT[Ch3Xmtr2]/slowdown;
-			// DR4B motors
-			motor[dr4b1] = vexRT[Ch3Xmtr2]/slowdown;
-			motor[dr4b2] = vexRT[Ch3Xmtr2]/slowdown;
-			motor[dr4b3] = vexRT[Ch3Xmtr2]/slowdown;
-			motor[dr4b4] = vexRT[Ch3Xmtr2]/slowdown;
+	while(1 == 1) {
+		joy_x = vexRT[Ch4];   // This is the left analog stick.
+		joy_y = vexRT[Ch3];
+		if (slowbutton == 0) {  // If slowbutton is pressed, speed decreases by 5. If not, it stays the same.
+			slowdown = 1;
 		}
+		if (slowbutton == 1) {
+			slowdown = 5;
+		}
+		setleft ((joy_y + joy_x)/slowdown);
+		setright ((joy_y - joy_x)/slowdown);
+
+		// Mobile Goal Lift motor
+		motor[lift] = vexRT[Ch2]/slowdown;
+		// DR4B Claw
+		motor[claw] = vexRT[Ch3Xmtr2]/slowdown;
+		// DR4B motors
+		motor[dr4b1] = vexRT[Ch3Xmtr2]/slowdown;
+		motor[dr4b2] = vexRT[Ch3Xmtr2]/slowdown;
+		motor[dr4b3] = vexRT[Ch3Xmtr2]/slowdown;
+		motor[dr4b4] = vexRT[Ch3Xmtr2]/slowdown;
 	}
+}
