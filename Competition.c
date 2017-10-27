a2ecb40134d653dbe6d15c95c6cfd67f89738341
VEX1868 Team K <30915286+VEX1868TeamK@users.noreply.github.com>
2017-10-26 19:16:38 -0700

    Tweaking Auton

--- Competition.c
+++ Competition.c
@@ -12,7 +12,6 @@
 
 #include "../Shared/Competition.h"
 
-int turnvar = 1;
 void setleft (int speed) {
 	motor[middleleft]=speed;
 	motor[twoleft]=speed;
@@ -21,69 +20,84 @@ void setleft (int speed) {
 void setright (int speed) {
 	motor[middleright]=speed;
 	motor[tworight]=speed;
-}
+	}
 
 void vertical (int speed) {
 	setleft (speed);
 	setright (speed);
 }
 
-void turn (int speed) {
+void turn (int speed, int turnvar) {
 	setleft (turnvar*speed);
 	setright (-1*turnvar*speed);
 }
 
 void autonomous () {
-	turn (1);
-	wait1Msec (2000);
-	vertical (100);
-	wait1Msec (400);
-	turn (1);
-	wait1Msec (2000);
-	turn (-1);
-	wait1Msec (2000);
-	vertical (100);
-	wait1Msec (1000);
-	motor [lift] = -100;
+	turn (100, -1);
 	wait1Msec (500);
-	motor [lift] = 100;
-	wait1Msec (1000);
-	vertical (50);
-	wait1Msec (500);
-	vertical (-100);
-	wait1Msec (1100);
-	turn (-1);
-	wait1Msec (1300);
 	vertical (127);
-	wait1Msec (3200);
+	wait1Msec (1500);
+	motor[lift]=120;
+	wait1Msec (100);
+	vertical (-127);
+	wait1Msec (800);
+	turn (100, 1);
+	wait1Msec (500);
+	vertical (127);
+	wait1Msec (100);
+	motor[lift]=120;
+	wait1Msec (100);
 }
 
+/*	turn (1);
+wait1Msec (2000);
+vertical (100);
+wait1Msec (400);
+turn (1);
+wait1Msec (2000);
+turn (-1);
+wait1Msec (2000);
+vertical (100);
+wait1Msec (1000);
+motor [lift] = -100;
+wait1Msec (500);
+motor [lift] = 100;
+wait1Msec (1000);
+vertical (50);
+wait1Msec (500);
+vertical (-100);
+wait1Msec (1100);
+turn (-1);
+wait1Msec (1300);
+vertical (127);
+wait1Msec (3200); */
+
 void joystick () {
-	int joy_x;            // will hold the X value of the analog stick (choices below)
-	int joy_y;            // will hold the Y value of the analog stick (choices below)
-	int slowbutton = vexRT[Btn5U];       // either 0 or 1 (pressed or not pressed)
-	int slowdown;
+int joy_x;            // will hold the X value of the analog stick (choices below)
+int joy_y;            // will hold the Y value of the analog stick (choices below)
+int slowbutton = vexRT[Btn5U];       // either 0 or 1 (pressed or not pressed)
+int slowdown;
 
-	while(1 == 1) {
-		joy_x = vexRT[Ch4];   // This is the left analog stick.
-		joy_y = vexRT[Ch3];
-		if (slowbutton == 0) {  // If slowbutton is pressed, speed decreases by 5. If not, it stays the same.
-			slowdown = 1;
-		}
-		if (slowbutton == 1) {
-			slowdown = 5;
-		}
-		setleft ((joy_y + joy_x)/slowdown);
-		setright ((joy_y - joy_x)/slowdown);
-
-		// Mobile Goal Lift motor
-		motor[lift] = vexRT[Ch2]/slowdown;
-		// DR4B Claw
-		motor[claw] = vexRT[Ch3Xmtr2]/slowdown;
-		// DR4B motors
-		motor[dr4b1] = vexRT[Ch2Xmtr2]/slowdown;
-		motor[dr4b2] = vexRT[Ch2Xmtr2]/slowdown;
-		motor[dr4b3] = vexRT[Ch2Xmtr2]/slowdown;
-		motor[dr4b4] = vexRT[Ch2Xmtr2]/slowdown;
+while(1 == 1) {
+	joy_x = vexRT[Ch4];   // This is the left analog stick.
+	joy_y = vexRT[Ch3];
+	if (slowbutton == 0) {  // If slowbutton is pressed, speed decreases by 5. If not, it stays the same.
+		slowdown = 1;
 	}
+	if (slowbutton == 1) {
+		slowdown = 5;
+	}
+	setleft ((joy_y + joy_x)/slowdown);
+	setright ((joy_y - joy_x)/slowdown);
+
+	// Mobile Goal Lift motor
+	motor[lift] = vexRT[Ch2]/slowdown;
+	// DR4B Claw
+	motor[claw] = vexRT[Ch3Xmtr2]/slowdown;
+	// DR4B motors
+	motor[dr4b1] = vexRT[Ch2Xmtr2]/slowdown;
+	motor[dr4b2] = vexRT[Ch2Xmtr2]/slowdown;
+	motor[dr4b3] = vexRT[Ch2Xmtr2]/slowdown;
+	motor[dr4b4] = vexRT[Ch2Xmtr2]/slowdown;
+}
 }
